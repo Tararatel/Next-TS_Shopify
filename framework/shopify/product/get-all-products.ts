32e982359e7270feafc516a562d551cec54ba3f0
Aleksey Sidorov <alekseysidorov.28@gmail.com>
2022-07-29 16:19:25 +0300

    init project

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,18 @@
+import fetchApi from '../utils/fetch-api';
+import getAllProductsQuery from '../utils/queries/get-all-products';
+import { ProductConnection } from '../schema';
+
+type ReturnType = {
+	products: ProductConnection;
+};
+
+const getAllProducts = async (): Promise<any> => {
+	const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery });
+	const products =
+		data.products.edges.map(({ node: product }) => {
+			return product;
+		}) ?? [];
+	return products;
+};
+
+export default getAllProducts;
